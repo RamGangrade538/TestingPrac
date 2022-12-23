335f9f1c79e55bbb001c9623359305b4fb66eedf
RamGangrade538 <118721796+RamGangrade538@users.noreply.github.com>
2022-12-23 15:28:01 +0530

    Add files via upload
    Check all the hyper links

---
+++ Hyperlnk.java
@@ -0,0 +1,42 @@
+package EroomrentOwnerg;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Hyperlnk {
+
+	public static void main(String[] args) {
+		
+			System.setProperty("webdriver.chrome.driver","C:\\Users\\aaa\\Downloads\\chromedriver_win32\\chromedriver.exe");
+			WebDriver driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.get("https://www.hdfcergo.com");
+			List<WebElement> b= driver.findElements(By.tagName("a"));
+			
+			int sizeb = b.size();
+			System.out.println();
+			for(int i=1;i<sizeb;i++)
+			{
+				WebElement a=b.get(i);
+				String c = a.getText();
+				System.out.println(c);	
+			}
+				
+			
+				
+				
+		/*
+		  List<WebElement> b = (List<WebElement>) driver.findElement(By.tagName("a"));		 int sizeb=b.size(); for(int i=1;i<sizeb;i++) WebElement = b.get(i); String c=
+		 a.getText(); System.out.println(c);
+		 */
+			
+			
+			
+			
+	}
+
+}
